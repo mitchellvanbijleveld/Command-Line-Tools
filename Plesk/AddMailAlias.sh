ffb33767aeb9fe479f18d40d6873af2d5b578db2
mitchellvanbijleveld <hello@mitchellvanbijleveld.dev>
2024-10-22 20:57:51 +0200

    Create Plesk/AddMailAlias.sh

---
+++ Plesk/AddMailAlias.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+VAR_UTILITY="Plesk"
+VAR_UTILITY_SCRIPT="AddMailAlias"
+VAR_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="echo plesk PrintMessage"
+
+VAR_EMAIL_ADDRESS=$2
+VAR_EMAIL_ADDRESS_ALIAS=$1
+
+if [[ $VAR_EMAIL_ADDRESS == "" ]]; then
+    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Please provide an email address!"
+    exit 1
+fi
+
+if [[ $VAR_EMAIL_ADDRESS_ALIAS == "" ]]; then
+    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Please provide an alias for this email address!"
+    exit 1
+fi
+
+VAR_EMAIL_ADDRESS_NEW_WITH_ALIAS="$VAR_EMAIL_ADDRESS_ALIAS@$(echo "$VAR_EMAIL_ADDRESS" | awk -F '@' '{print $2}')"
+
+PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Adding alias '$VAR_EMAIL_ADDRESS_ALIAS' to email address '$VAR_EMAIL_ADDRESS'..."
+PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "New email address / alias: '$VAR_EMAIL_ADDRESS_NEW_WITH_ALIAS'."
+plesk bin mail -u "$VAR_EMAIL_ADDRESS" -aliases "add:$VAR_EMAIL_ADDRESS_ALIAS"
