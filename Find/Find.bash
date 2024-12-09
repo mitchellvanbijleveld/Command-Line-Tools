eb26ce3c564c677e00014ebc62e18b4192331bee
Mitchell van Bijleveld <hello@mitchellvanbijleveld.dev>
2024-12-10 00:57:41 +0100

    Allow arguments to change search dir or only search for files or folders

--- Find/Find.bash
+++ Find/Find.bash
@@ -7,7 +7,7 @@
 VAR_UTILITY="Find"
 VAR_UTILITY_SCRIPT="Find"
 VAR_UTILITY_SCRIPT_VERSION="2024.12.10-0005"
-VAR_UTILITY_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="clear find PrintMessage sudo"
+VAR_UTILITY_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="clear echo find PrintMessage sed sudo"
 ####################################################################################################
 # UTILITY SCRIPT INFO - Find/Find
 ####################################################################################################
@@ -21,12 +21,53 @@ VAR_UTILITY_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="clear find PrintMessage sudo"
 ####################################################################################################
 # VARIABLES
 ####################################################################################################
+VAR_FLAGS="--DIRS-ONLY --FILES-ONLY --SEARCH-DIR"
+VAR_ARGS=($@)
+#
+VAR_DIRS_ONLY=0
+VAR_FILES_ONLY=0
+#
 VAR_SEARCH_QUERY=$@
 VAR_SEARCH_DIR="/"
 ####################################################################################################
 # VARIABLES
 ####################################################################################################
 ####################################################################################################
+for var_argument in $VAR_SEARCH_QUERY; do
+    shift
+    if [[ $VAR_FLAGS =~ (^|[[:space:]])$var_argument([[:space:]]|$) ]]; then
+        VAR_SEARCH_QUERY=$(echo $VAR_SEARCH_QUERY | sed -E "s/$var_argument[ ]*//")
+    fi
+    #
+    case $var_argument in
+        "--DIRS-ONLY") 
+            if [[ $VAR_FILES_ONLY -eq 1 ]]; then
+                PrintMessage "FATAL" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Argument '--DIRS-ONLY' was not accepted because '--FILES-ONLY' was already set. Exiting..."
+                exit 1
+            else
+                VAR_DIRS_ONLY=1
+            fi
+            ;;
+        "--FILES-ONLY") 
+            if [[ $VAR_DIRS_ONLY -eq 1 ]]; then
+                PrintMessage "FATAL" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Argument '--FILES-ONLY' was not accepted because '--DIRS-ONLY' was already set. Exiting..."
+                exit 1
+            else
+                VAR_FILES_ONLY=1
+            fi
+            ;;
+        "--SEARCH-DIR")
+            if [[ ! -d $1 ]]; then
+                PrintMessage "FATAL" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Argument '--SEARCH-DIR' was not given a valid existing directory. Exiting..."
+                exit 1
+            fi
+            VAR_SEARCH_DIR=$1
+            VAR_SEARCH_QUERY=$(echo $VAR_SEARCH_QUERY | sed -E "s|[ ]*$VAR_SEARCH_DIR||")
+            ;;
+        "--"*) PrintMessage "FATAL" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Invalid option given. Exiting..."; exit 1;;
+    esac
+done
+#
 if [[ $VAR_SEARCH_QUERY == "" ]]; then
     PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Search query not set! Exiting..."
     exit 1
@@ -34,7 +75,16 @@ fi
 #
 $(which clear)
 #
-PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Searching in '$VAR_SEARCH_DIR' for '$VAR_SEARCH_QUERY'..."
-PrintMessage
-#
-$(which sudo) $(which find) "$VAR_SEARCH_DIR" -iname "*$VAR_SEARCH_QUERY*"
+if [[ $VAR_DIRS_ONLY -eq 1 ]]; then
+    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Searching for directories only in '$VAR_SEARCH_DIR' for '$VAR_SEARCH_QUERY'..."
+    PrintMessage
+    $(which sudo) $(which find) "$VAR_SEARCH_DIR" -iname "*$VAR_SEARCH_QUERY*" -type d
+elif [[ $VAR_FILES_ONLY -eq 1 ]]; then
+    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Searching for files only in '$VAR_SEARCH_DIR' for '$VAR_SEARCH_QUERY'..."
+    PrintMessage
+    $(which sudo) $(which find) "$VAR_SEARCH_DIR" -iname "*$VAR_SEARCH_QUERY*" -type f
+else
+    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Searching in '$VAR_SEARCH_DIR' for '$VAR_SEARCH_QUERY'..."
+    PrintMessage
+    $(which sudo) $(which find) "$VAR_SEARCH_DIR" -iname "*$VAR_SEARCH_QUERY*"
+fi
