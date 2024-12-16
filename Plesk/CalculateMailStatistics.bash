4cc9fbc66420385108832db35fa7d435cef29489
Mitchell van Bijleveld <hello@mitchellvanbijleveld.dev>
2024-12-16 23:03:07 +0100

    Make printing filesystem statistics optional, print real summary instead

--- Plesk/CalculateMailStatistics.bash
+++ Plesk/CalculateMailStatistics.bash
@@ -41,6 +41,17 @@ for var_argument in "$@"; do
     var_argument_CAPS=$(echo $var_argument | tr '[:lower:]' '[:upper:]')
     #
     case $var_argument_CAPS in
+        "--PRINT-STATISTICS")
+            while [[ $2 != "--"* && $2 != "" ]]; do
+                case $2 in
+                    "filesystem") 
+                        PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Script will print filesystem statistics..."
+                        PRINT_STATISTICS_FILESYSTEM=1
+                    ;;
+                esac
+                shift
+            done
+        ;;
         "--"*)
             PrintMessage "FATAL" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Invalid option given. Exiting..."
             exit 1
@@ -80,12 +91,12 @@ CountMail_FileSystem(){
 PrintStatistics_FileSystem(){
     # $1 = DOMAIN NAME
     # $2 - EMAIL ADDRESS
-    echo "    $(printf "%-7s\n" Total): $(printf "%6s\n" $(cat "$TMP_DIR/$1/$2/ALL"))"
-    echo "    $(printf "%-7s\n" Drafts): $(printf "%6s\n" $(cat "$TMP_DIR/$1/$2/DRAFTS"))"
-    echo "    $(printf "%-7s\n" Notes): $(printf "%6s\n" $(cat "$TMP_DIR/$1/$2/NOTES"))"
-    echo "    $(printf "%-7s\n" Sent): $(printf "%6s\n" $(cat "$TMP_DIR/$1/$2/SENT"))"
-    echo "    $(printf "%-7s\n" Spam): $(printf "%6s\n" $(cat "$TMP_DIR/$1/$2/SPAM"))"
-    echo "    $(printf "%-7s\n" Inbox): $(printf "%6s\n" $(cat "$TMP_DIR/$1/$2/INBOX"))"
+    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "    Total  : $(printf "%6s\n" $(cat "$TMP_DIR/$1/$2/___ALL"))"
+    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "    Drafts : $(printf "%6s\n" $(cat "$TMP_DIR/$1/$2/DRAFTS"))"
+    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "    Notes  : $(printf "%6s\n" $(cat "$TMP_DIR/$1/$2/_NOTES"))"
+    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "    Sent   : $(printf "%6s\n" $(cat "$TMP_DIR/$1/$2/__SENT"))"
+    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "    Spam   : $(printf "%6s\n" $(cat "$TMP_DIR/$1/$2/__SPAM"))"
+    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "    Inbox  : $(printf "%6s\n" $(cat "$TMP_DIR/$1/$2/_INBOX"))"
 }
 ####################################################################################################
 # FUNCTIONS
@@ -107,6 +118,13 @@ PrintStatistics_FileSystem(){
 ##################################################
 TMP_DIR=$(mktemp -d -t "mitchellvanbijleveld-$VAR_UTILITY-$VAR_UTILITY_SCRIPT-FS.XXXXXXXX")
 PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Created Temporary Directory: $TMP_DIR"
+#
+echo 0 > "$TMP_DIR/___ALL"
+echo 0 > "$TMP_DIR/DRAFTS"
+echo 0 > "$TMP_DIR/_NOTES"
+echo 0 > "$TMP_DIR/__SENT"
+echo 0 > "$TMP_DIR/__SPAM"
+echo 0 > "$TMP_DIR/_INBOX"
 #
 for var_domain in $VAR_MAIL_DIRECTORY/*; do
     #
@@ -137,12 +155,19 @@ for var_domain in $VAR_MAIL_DIRECTORY/*; do
         VAR_MAIL_COUNT___SPAM=$(CountMail_FileSystem "$var_domain_basename" "$var_email_address_basename" ".Spam")
         VAR_MAIL_COUNT__INBOX=$((VAR_MAIL_COUNT____ALL-VAR_MAIL_COUNT_DRAFTS-VAR_MAIL_COUNT__NOTES-VAR_MAIL_COUNT___SENT-VAR_MAIL_COUNT___SPAM))
         #
-        echo $VAR_MAIL_COUNT____ALL > "$TMP_DIR_EMAIL_ADDRESS/ALL"
+        echo $VAR_MAIL_COUNT____ALL > "$TMP_DIR_EMAIL_ADDRESS/___ALL"
         echo $VAR_MAIL_COUNT_DRAFTS > "$TMP_DIR_EMAIL_ADDRESS/DRAFTS"
-        echo $VAR_MAIL_COUNT__NOTES > "$TMP_DIR_EMAIL_ADDRESS/NOTES"
-        echo $VAR_MAIL_COUNT___SENT > "$TMP_DIR_EMAIL_ADDRESS/SENT"
-        echo $VAR_MAIL_COUNT___SPAM > "$TMP_DIR_EMAIL_ADDRESS/SPAM"
-        echo $VAR_MAIL_COUNT__INBOX > "$TMP_DIR_EMAIL_ADDRESS/INBOX"
+        echo $VAR_MAIL_COUNT__NOTES > "$TMP_DIR_EMAIL_ADDRESS/_NOTES"
+        echo $VAR_MAIL_COUNT___SENT > "$TMP_DIR_EMAIL_ADDRESS/__SENT"
+        echo $VAR_MAIL_COUNT___SPAM > "$TMP_DIR_EMAIL_ADDRESS/__SPAM"
+        echo $VAR_MAIL_COUNT__INBOX > "$TMP_DIR_EMAIL_ADDRESS/_INBOX"
+        #
+        echo $(($(cat "$TMP_DIR/___ALL")+$VAR_MAIL_COUNT____ALL)) > "$TMP_DIR/___ALL"
+        echo $(($(cat "$TMP_DIR/DRAFTS")+$VAR_MAIL_COUNT_DRAFTS)) > "$TMP_DIR/DRAFTS"
+        echo $(($(cat "$TMP_DIR/_NOTES")+$VAR_MAIL_COUNT__NOTES)) > "$TMP_DIR/_NOTES"
+        echo $(($(cat "$TMP_DIR/__SENT")+$VAR_MAIL_COUNT___SENT)) > "$TMP_DIR/__SENT"
+        echo $(($(cat "$TMP_DIR/__SPAM")+$VAR_MAIL_COUNT___SPAM)) > "$TMP_DIR/__SPAM"
+        echo $(($(cat "$TMP_DIR/_INBOX")+$VAR_MAIL_COUNT__INBOX)) > "$TMP_DIR/_INBOX"
     done
 done
 ##################################################
@@ -156,28 +181,50 @@ done
 #
 ##################################################
 ##################################################
-# PRINT SUMMARY
+# PRINT STATISTICS FILESYSTEM
 ##################################################
-for var_domain in $TMP_DIR/*; do
-    if [[ -d $var_domain ]]; then
-        var_domain_basename=$(basename $var_domain)
-        PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Domain Name: $var_domain_basename"
-    else
-        continue
-    fi
-    #
-    for var_email_address in $var_domain/*; do
-        if [[ -d $var_email_address ]]; then
-            var_email_address_basename=$(basename $var_email_address)
-            PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  - Email Address: $var_email_address_basename@$var_domain_basename"
+if [[ $PRINT_STATISTICS_FILESYSTEM -eq 1 ]]; then
+    for var_domain in $TMP_DIR/*; do
+        if [[ -d $var_domain ]]; then
+            var_domain_basename=$(basename $var_domain)
+            PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Domain Name: $var_domain_basename"
         else
-            PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  No email address found."
             continue
         fi
-        PrintStatistics_FileSystem $var_domain_basename $var_email_address_basename
+        #
+        for var_email_address in $var_domain/*; do
+            if [[ -d $var_email_address ]]; then
+                var_email_address_basename=$(basename $var_email_address)
+                PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  - Email Address: $var_email_address_basename@$var_domain_basename"
+            else
+                PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  No email address found."
+                continue
+            fi
+            PrintStatistics_FileSystem $var_domain_basename $var_email_address_basename
+        done
+        PrintMessage
     done
-    PrintMessage
-done
+fi
+##################################################
+# PRINT STATISTICS FILESYSTEM
+##################################################
+##################################################
+#
+#
+#
+#
+#
+##################################################
+##################################################
+# PRINT SUMMARY
+##################################################
+PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Mail Statistics Summary"
+PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  - Total  : $(printf "%6s\n" $(cat "$TMP_DIR/___ALL"))"
+PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  - Drafts : $(printf "%6s\n" $(cat "$TMP_DIR/DRAFTS"))"
+PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  - Notes  : $(printf "%6s\n" $(cat "$TMP_DIR/_NOTES"))"
+PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  - Sent   : $(printf "%6s\n" $(cat "$TMP_DIR/__SENT"))"
+PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  - Spam   : $(printf "%6s\n" $(cat "$TMP_DIR/__SPAM"))"
+PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "  - Inbox  : $(printf "%6s\n" $(cat "$TMP_DIR/_INBOX"))"
 ##################################################
 # PRINT SUMMARY
 ##################################################
