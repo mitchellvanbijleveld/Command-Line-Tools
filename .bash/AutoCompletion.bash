369b15036e8d21520adba6bfc3ca4538f00bd931
Mitchell van Bijleveld <hello@mitchellvanbijleveld.dev>
2024-12-19 18:28:35 +0100

    Add the script arguments to be autocompleted as well

--- .bash/AutoCompletion.bash
+++ .bash/AutoCompletion.bash
@@ -65,19 +65,27 @@ done
 # FUNCTIONS
 ####################################################################################################
 AutoCompleteFunction() {
-    local current_word previous_word VAR_UTILITY_FOLDERS VAR_UTILITY_SCRIPT_FILES
+    #local current_word var_utility var_utility_script VAR_UTILITY_FOLDERS VAR_UTILITY_SCRIPT_FILES VAR_SCRIPT_OPTIONS
 
     current_word="${COMP_WORDS[COMP_CWORD]}"
-    previous_word="${COMP_WORDS[COMP_CWORD-1]}"
+    var_utility="${COMP_WORDS[1]}"
+    var_utility_script="${COMP_WORDS[2]}"
+
 
     if [[ $COMP_CWORD -eq 1 ]]; then
         VAR_UTILITY_FOLDERS=$(find "GLOBAL_VAR_DIR_INSTALLATION" -maxdepth 1 -type d ! -iname '.*' -exec basename {} \;)
         COMPREPLY=( $(compgen -W "$VAR_UTILITY_FOLDERS" -- "$current_word") )
 
     elif [[ $COMP_CWORD -eq 2 ]]; then
-        if [[ -d "GLOBAL_VAR_DIR_INSTALLATION/$previous_word" ]]; then
-            VAR_UTILITY_SCRIPT_FILES=$(find "GLOBAL_VAR_DIR_INSTALLATION/$previous_word" -maxdepth 1 -type f -iname "*.bash" -exec basename {} .bash \;)
+        if [[ -d "GLOBAL_VAR_DIR_INSTALLATION/$var_utility" ]]; then
+            VAR_UTILITY_SCRIPT_FILES=$(find "GLOBAL_VAR_DIR_INSTALLATION/$var_utility" -maxdepth 1 -type f -iname "*.bash" -exec basename {} .bash \;)
             COMPREPLY=( $(compgen -W "$VAR_UTILITY_SCRIPT_FILES" -- "$current_word") )
+        fi
+
+    elif [[ $COMP_CWORD -ge 3 ]]; then
+        if [[ -f "GLOBAL_VAR_DIR_INSTALLATION/$var_utility/$var_utility_script.bash" ]]; then
+            VAR_SCRIPT_OPTIONS=$(grep -Eo -- '--[a-zA-Z-]+' "GLOBAL_VAR_DIR_INSTALLATION/$var_utility/$var_utility_script.bash" | sort -u)
+            COMPREPLY=( $(compgen -W "$VAR_SCRIPT_OPTIONS" -- "$current_word") )
         fi
     fi
 }
