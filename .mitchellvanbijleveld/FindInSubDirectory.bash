de94e5c8274ce60f33b5f39c336f7d57db857507
Mitchell van Bijleveld <hello@mitchellvanbijleveld.dev>
2025-01-02 23:42:40 +0100

    Fix logging and die functions

--- .mitchellvanbijleveld/FindInSubDirectory.bash
+++ .mitchellvanbijleveld/FindInSubDirectory.bash
@@ -4,8 +4,8 @@
 ####################################################################################################
 # UTILITY SCRIPT INFO - .mitchellvanbijleveld/FindInSubDirectory
 ####################################################################################################
-VAR_UTILITY=".mitchellvanbijleveld"
-VAR_UTILITY_SCRIPT="FindInSubDirectory"
+#VAR_UTILITY=".bin"
+#VAR_UTILITY_SCRIPT="FindInSubDirectory"
 VAR_UTILITY_SCRIPT_VERSION="2025.01.02-0018"
 VAR_UTILITY_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="echo exit PrintMessage shift tr"
 ####################################################################################################
@@ -21,7 +21,8 @@ VAR_UTILITY_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="echo exit PrintMessage shift tr"
 ####################################################################################################
 # DEFAULT VARIABLES
 ####################################################################################################
-VAR_NAME=$(basename $0 | sed 's/.bash$//')
+VAR_SUB_DIRECTORY_BASE_NAME=$(basename $0 | sed 's/.bash$//')
+VAR_VAR_UTILITY_SAVED=$(basename $(dirname $0))
 ####################################################################################################
 # DEFAULT VARIABLES
 ####################################################################################################
@@ -36,27 +37,27 @@ VAR_NAME=$(basename $0 | sed 's/.bash$//')
 # PROCESS ARGUMENTS
 ####################################################################################################
 for var_argument in "$@"; do
-    PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Processing argument '$var_argument'..."
+    PrintMessage "DEBUG" ".bin" "FindInSubDirectory" "Processing argument '$var_argument'..."
     var_argument_CAPS=$(echo $var_argument | tr '[:lower:]' '[:upper:]')
     #
     case $var_argument_CAPS in
         "--SKIP-START-SCRIPT")
             VAR_SKIP_START=1
-            unset VAR_NAME
+            unset VAR_SUB_DIRECTORY_BASE_NAME
         ;;
         "--"*)
-            PrintMessage "FATAL" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Invalid option given. Exiting..."
+            PrintMessage "FATAL" ".bin" "FindInSubDirectory" "Invalid option given. Exiting..."
             exit 1
         ;;
         *)       
             if [[ $VAR_VAR_UTILITY == "" ]] && [[ $var_argument_CAPS != "--"* ]]; then
-                PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Setting '$var_argument' as VAR_VAR_UTILITY..."
+                PrintMessage "DEBUG" ".bin" "FindInSubDirectory" "Setting '$var_argument' as VAR_VAR_UTILITY..."
                 VAR_VAR_UTILITY=$var_argument_CAPS
             elif [[ $VAR_VAR_UTILITY_SCRIPT == "" ]] && [[ $var_argument_CAPS != "--"* ]]; then
-                PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Setting '$var_argument' as VAR_VAR_UTILITY_SCRIPT..."
+                PrintMessage "DEBUG" ".bin" "FindInSubDirectory" "Setting '$var_argument' as VAR_VAR_UTILITY_SCRIPT..."
                 VAR_VAR_UTILITY_SCRIPT=$var_argument_CAPS
             else
-                PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Ignoring argument '$var_argument'. Forwarding argument to $VAR_NAME Utility Script..."
+                PrintMessage "DEBUG" ".bin" "FindInSubDirectory" "Ignoring argument '$var_argument'. Forwarding argument to $VAR_UTILITY Utility Script..."
                 VAR_UTILITY_SCRIPT_ARGUMENTS+=("$var_argument")
             fi
         ;;
@@ -96,15 +97,15 @@ done
 ##################################################
 # FIND * UTILITY FOLDER PATH
 ##################################################
-PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Checking variable *_VAR_UTILITY with value '$VAR_VAR_UTILITY'..."
+PrintMessage "DEBUG" ".bin" "FindInSubDirectory" "Checking variable *_VAR_UTILITY with value '$VAR_VAR_UTILITY'..."
 if [[ $VAR_VAR_UTILITY == "" ]]; then
-    die_UtilityNotFound "No Utility was provided!" "$VAR_NAME"
+    die_UtilityNotFound "No $VAR_VAR_UTILITY_SAVED Utility was provided!" "$VAR_SUB_DIRECTORY_BASE_NAME"
 fi
 #
-if FindUtilityFolderPath "$GLOBAL_VAR_DIR_INSTALLATION/$VAR_NAME" "$VAR_VAR_UTILITY"; then
-    PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "$1 Utility Folder '$VAR_VAR_UTILITY' found!"
+if FindUtilityFolderPath "$GLOBAL_VAR_DIR_INSTALLATION/$VAR_SUB_DIRECTORY_BASE_NAME" "$VAR_VAR_UTILITY"; then
+    PrintMessage "DEBUG" ".bin" "FindInSubDirectory" "$1 Utility Folder '$VAR_VAR_UTILITY' found!"
 else
-    die_UtilityNotFound "The $VAR_NAME Utility Folder for '$VAR_VAR_UTILITY' was not found" "$VAR_NAME"
+    die_UtilityNotFound "The $VAR_VAR_UTILITY_SAVED Utility Folder for '$VAR_VAR_UTILITY' was not found" "$VAR_SUB_DIRECTORY_BASE_NAME"
 fi
 ##################################################
 # FIND * UTILITY FOLDER PATH
@@ -120,15 +121,15 @@ fi
 # FIND UTILITY SCRIPT * FILE PATH
 ##################################################
 if FindUtilityScriptFilePath "$VAR_UTILITY_FOLDER_PATH" "$VAR_VAR_UTILITY_SCRIPT"; then
-    PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "$VAR_NAME Utility Script File '$VAR_VAR_UTILITY_SCRIPT' found in Utility '$VAR_VAR_UTILITY'!"
+    PrintMessage "DEBUG" ".bin" "FindInSubDirectory" "$VAR_UTILITY Utility Script File '$VAR_VAR_UTILITY_SCRIPT' found in Utility '$VAR_VAR_UTILITY'!"
 elif FindUtilityScriptFilePath "$VAR_UTILITY_FOLDER_PATH" "$VAR_VAR_UTILITY"; then
-    PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Utility Script '$VAR_VAR_UTILITY' found in Utility '$VAR_VAR_UTILITY'!"
-    PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Changing variable 'VAR_VAR_UTILITY_SCRIPT' from '$VAR_VAR_UTILITY_SCRIPT' to '$VAR_VAR_UTILITY'..."
+    PrintMessage "DEBUG" ".bin" "FindInSubDirectory" "Utility Script '$VAR_VAR_UTILITY' found in Utility '$VAR_VAR_UTILITY'!"
+    PrintMessage "DEBUG" ".bin" "FindInSubDirectory" "Changing variable 'VAR_VAR_UTILITY_SCRIPT' from '$VAR_VAR_UTILITY_SCRIPT' to '$VAR_VAR_UTILITY'..."
     VAR_VAR_UTILITY_SCRIPT=$VAR_VAR_UTILITY;
 elif [[ $VAR_VAR_UTILITY_SCRIPT == "" ]]; then
-    die_UtilityScriptNotFound "No Utility Script specified!" "$VAR_NAME"
+    die_UtilityScriptNotFound "No $VAR_VAR_UTILITY_SAVED Utility Script specified!" "$VAR_SUB_DIRECTORY_BASE_NAME"
 else
-    die_UtilityScriptNotFound "Utility Script '$VAR_VAR_UTILITY_SCRIPT' not found within Utility '$VAR_VAR_UTILITY'!" "$VAR_NAME"
+    die_UtilityScriptNotFound "$VAR_VAR_UTILITY_SAVED Utility Script '$VAR_VAR_UTILITY_SCRIPT' not found within Utility '$VAR_VAR_UTILITY'!" "$VAR_SUB_DIRECTORY_BASE_NAME"
 fi
 ##################################################
 # FIND UTILITY SCRIPT * FILE PATH
@@ -143,7 +144,7 @@ fi
 ##################################################
 # EXPORT * UTILITY SCRIPT VARIABLES
 ##################################################
-PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Exporting Utility Script Variables..."
+PrintMessage "DEBUG" ".bin" "FindInSubDirectory" "Exporting Utility Script Variables..."
 export UTILITY_SCRIPT_VAR_DIR_ETC="$GLOBAL_VAR_DIR_ETC/$(basename $(dirname $VAR_UTILITY_SCRIPT_FILE_PATH))/$(basename $VAR_UTILITY_SCRIPT_FILE_PATH | sed 's/.bash$//')"
 #export UTILITY_SCRIPT_VAR_DIR_TMP="$GLOBAL_VAR_DIR_TMP/$(basename $(dirname $VAR_UTILITY_SCRIPT_FILE_PATH))/$(basename $VAR_UTILITY_SCRIPT_FILE_PATH | sed 's/.bash$//')"
 export UTILITY_SCRIPT_VAR_DIR_TMP=$(mktemp -d "mitchellvanbijleveld-$VAR_UTILITY-$VAR_UTILITY_SCRIPT.XXXXXXXX" --tmpdir)
@@ -162,8 +163,8 @@ VAR_VAR_UTILITY_SCRIPT=$(basename $VAR_UTILITY_SCRIPT_FILE_PATH | sed 's/.bash$/
 ##################################################
 # CREATE * UTILITY SCRIPT DIRECTORIES
 ##################################################
-PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Creating directories..."
-PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Creating directory '$UTILITY_SCRIPT_VAR_DIR_ETC'..."
+PrintMessage "DEBUG" ".bin" "FindInSubDirectory" "Creating directories..."
+PrintMessage "DEBUG" ".bin" "FindInSubDirectory" "Creating directory '$UTILITY_SCRIPT_VAR_DIR_ETC'..."
 mkdir -p $UTILITY_SCRIPT_VAR_DIR_ETC
 ##################################################
 # CREATE * UTILITY SCRIPT DIRECTORIES
@@ -179,8 +180,8 @@ mkdir -p $UTILITY_SCRIPT_VAR_DIR_ETC
 # START * UTILITY SCRIPT
 ##################################################
 if [[ $VAR_SKIP_START -eq 1 ]]; then
-    PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Not starting Utility Script..."
+    PrintMessage "DEBUG" ".bin" "FindInSubDirectory" "Not starting Utility Script..."
 else
-    PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Starting Configure Utility '$VAR_VAR_UTILITY' Script '$VAR_VAR_UTILITY_SCRIPT'..."
+    PrintMessage "DEBUG" ".bin" "FindInSubDirectory" "Starting Configure Utility '$VAR_VAR_UTILITY' Script '$VAR_VAR_UTILITY_SCRIPT'..."
     $(which bash) $VAR_UTILITY_SCRIPT_FILE_PATH "${VAR_UTILITY_SCRIPT_ARGUMENTS[@]}"
 fi
