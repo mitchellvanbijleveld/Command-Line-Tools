130f8abf3ed4320aff834aaa8615529c9e0ce1bf
Mitchell van Bijleveld <hello@mitchellvanbijleveld.dev>
2025-01-19 18:53:59 +0100

    Use PrintMessageV2 instead of PrintMessage

--- Find/Find.bash
+++ Find/Find.bash
@@ -6,8 +6,8 @@
 ####################################################################################################
 VAR_UTILITY="Find"
 VAR_UTILITY_SCRIPT="Find"
-VAR_UTILITY_SCRIPT_VERSION="2024.12.22-0011"
-VAR_UTILITY_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="clear echo find mktemp PrintMessage sed shift sudo tr wc which"
+VAR_UTILITY_SCRIPT_VERSION="2025.01.19-1853"
+VAR_UTILITY_SCRIPT_REQUIRED_COMMAND_LINE_TOOLS="clear echo find mktemp PrintMessageV2 sed shift sudo tr wc which"
 ####################################################################################################
 # UTILITY SCRIPT INFO - Find/Find
 ####################################################################################################
@@ -38,13 +38,13 @@ VAR_SEARCH_DIR=$HOME
 # PROCESS ARGUMENTS
 ####################################################################################################
 for var_argument in "$@"; do
-    PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Processing argument '$var_argument'..."
+    PrintMessageV2 "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Processing argument '$var_argument'..."
     var_argument_CAPS=$(echo $var_argument | tr '[:lower:]' '[:upper:]')
     #
     case $var_argument_CAPS in
         "--DIRS-ONLY" | "--SKIP-FILES") 
             if [[ $VAR_FILES_ONLY -eq 1 ]]; then
-                PrintMessage "FATAL" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Argument '--DIRS-ONLY' was not accepted because '--FILES-ONLY' was already set. Exiting..."
+                PrintMessageV2 "FATAL" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Argument '--DIRS-ONLY' was not accepted because '--FILES-ONLY' was already set. Exiting..."
                 exit 1
             else
                 VAR_DIRS_ONLY=1
@@ -55,7 +55,7 @@ for var_argument in "$@"; do
         ;;
         "--FILES-ONLY" | "--SKIP-DIRS" | "--SKIP-DIRECTORIES") 
             if [[ $VAR_DIRS_ONLY -eq 1 ]]; then
-                PrintMessage "FATAL" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Argument '--FILES-ONLY' was not accepted because '--DIRS-ONLY' was already set. Exiting..."
+                PrintMessageV2 "FATAL" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Argument '--FILES-ONLY' was not accepted because '--DIRS-ONLY' was already set. Exiting..."
                 exit 1
             else
                 VAR_FILES_ONLY=1
@@ -63,37 +63,37 @@ for var_argument in "$@"; do
         ;;
         "--SEARCH-DIR" | "--SEARCH-DIRECTORY")
             if [[ $VAR_SEARCH_DIR != $HOME ]]; then
-                PrintMessage "WARN" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Argument '--SEARCH-DIR' should only be set once. Overriding..,"
+                PrintMessageV2 "WARNING" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Argument '--SEARCH-DIR' should only be set once. Replacing existing value..,"
             fi
             VAR_SEARCH_DIR=$2           
             if [[ ! -d $VAR_SEARCH_DIR ]]; then
-                PrintMessage "FATAL" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Argument '--SEARCH-DIR' was not given a valid existing directory ('$VAR_SEARCH_DIR'). Exiting..."
+                PrintMessageV2 "FATAL" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Argument '--SEARCH-DIR' was not given a valid existing directory ('$VAR_SEARCH_DIR'). Exiting..."
                 exit 1
             fi
-            PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Set search directory to '$VAR_SEARCH_DIR'..."
+            PrintMessageV2 "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Set search directory to '$VAR_SEARCH_DIR'..."
             if [[ $VAR_SEARCH_DIR == "." || $VAR_SEARCH_DIR == *".."* ]]; then
-                PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Search directory real path is '$(realpath $VAR_SEARCH_DIR)'..."
+                PrintMessageV2 "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Search directory real path is '$(realpath $VAR_SEARCH_DIR)'..."
             fi
         ;;
         "--SORT" | "--SORT-OUTPUT")
-            PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Sorting output when done..."
+            PrintMessageV2 "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Sorting output when done..."
             SORT_OUTPUT=1
         ;;
         "--SUPPRESS-ERRORS")
-            PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Suppressing warnings and errors in output..."
+            PrintMessageV2 "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Suppressing warnings and errors in output..."
             SUPPRESS_PERMISSION_DENIED=1
         ;;
         "--EXECUTE-COMMAND" | "--RUN-COMMAND")
             if type $2 &> /dev/null && [[ $2 == *"{}"* ]]; then
-                PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Running command '$2' for every found item..."
+                PrintMessageV2 "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Running command '$2' for every found item..."
                 VAR_RUN_COMMAND=$2
             else
-                PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Given command '$2' can not be executed. Command should include '{}'. Exiting..."
+                PrintMessageV2 "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Given command '$2' can not be executed. Command should include '{}'. Exiting..."
                 exit 1
             fi
         ;;
         "--WILDCARD" | "--WILDCARD-SEARCH")
-            PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Searching for search string with wildcards before and after..."
+            PrintMessageV2 "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Searching for search string with wildcards before and after..."
             SEARCH_WILDCARD=1
         ;;
         "--"*)
@@ -101,10 +101,10 @@ for var_argument in "$@"; do
         ;;
         *)
             if [[ $VAR_SEARCH_QUERY == "" ]] || [[ $VAR_SEARCH_QUERY == $VAR_SEARCH_DIR ]]; then
-                PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Setting '$var_argument' as search string..."
+                PrintMessageV2 "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Setting '$var_argument' as search string..."
                 VAR_SEARCH_QUERY=$var_argument
             else
-                PrintMessage "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Ignoring argument '$var_argument'..."
+                PrintMessageV2 "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Ignoring argument '$var_argument'..."
             fi
         ;;
     esac
@@ -139,10 +139,10 @@ done
 # START UTILITY SCRIPT
 ####################################################################################################
 if [[ $VAR_SEARCH_QUERY == "" ]]; then
-    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Search query not set! Exiting..."
+    PrintMessageV2 "FATAL" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Search query not set! Exiting..."
     exit 1
 elif [[ $VAR_SEARCH_QUERY == $VAR_SEARCH_DIR ]]; then
-    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Search query can not be the same as search directory! Exiting..."
+    PrintMessageV2 "FATAL" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Search query can not be the same as search directory! Exiting..."
     exit 1
 fi
 #
@@ -150,7 +150,7 @@ if [[ $SEARCH_WILDCARD -eq 1 ]]; then
     VAR_SEARCH_QUERY="*$VAR_SEARCH_QUERY*"
 fi
 #
-$(which clear)
+# $(which clear)
 #
 if [[ $VAR_EXCLUDE_DIR != "" ]]; then
     VAR_FIND_COMMAND="'$VAR_SEARCH_DIR' -path '$VAR_EXCLUDE_DIR' -prune -o -iname '$VAR_SEARCH_QUERY' -print"
@@ -158,23 +158,21 @@ else
     VAR_FIND_COMMAND="'$VAR_SEARCH_DIR' -iname '$VAR_SEARCH_QUERY'"
 fi
 #
-
-#
 if [[ $VAR_DIRS_ONLY -eq 1 ]]; then
-    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Searching for directories only in '$VAR_SEARCH_DIR' for '$VAR_SEARCH_QUERY'..."
+    PrintMessageV2 "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Searching for directories only in '$VAR_SEARCH_DIR' for '$VAR_SEARCH_QUERY'..."
     VAR_FIND_COMMAND="$VAR_FIND_COMMAND -type d"
 elif [[ $VAR_FILES_ONLY -eq 1 ]]; then
-    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Searching for files only in '$VAR_SEARCH_DIR' for '$VAR_SEARCH_QUERY'..."
+    PrintMessageV2 "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Searching for files only in '$VAR_SEARCH_DIR' for '$VAR_SEARCH_QUERY'..."
     VAR_FIND_COMMAND="$VAR_FIND_COMMAND -type f"
 else
-    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Searching in '$VAR_SEARCH_DIR' for '$VAR_SEARCH_QUERY'..."
+    PrintMessageV2 "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Searching in '$VAR_SEARCH_DIR' for '$VAR_SEARCH_QUERY'..."
 fi
 #
 if [[ $VAR_RUN_COMMAND != "" ]]; then
     VAR_FIND_COMMAND="$VAR_FIND_COMMAND -exec $VAR_RUN_COMMAND \;"
 fi
 #
-PrintMessage
+PrintMessageV2
 #
 ##################################################
 ##################################################
@@ -190,19 +188,20 @@ if [[ $SUPPRESS_PERMISSION_DENIED -eq 1 ]]; then
     VAR_FIND_COMMAND="$VAR_FIND_COMMAND | grep -Ev '$SUPPRESS_STRING'"
 fi
 #
-PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" $(which find) "$VAR_FIND_COMMAND"
-##################################################
-# PREPARE AND RUN FIND COMMAND
-##################################################
-##################################################
+VAR_TMP_FILE_COMMAND_OUTPUT=$(mktemp "mitchellvanbijleveld-$VAR_UTILITY-$VAR_UTILITY_SCRIPT.XXXXXXXX" --tmpdir)
+PrintMessageV2 "DEBUG" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Saving find output to: $VAR_TMP_FILE_COMMAND_OUTPUT"
 #
-PrintMessage
+VAR_FIND_COMMAND="$VAR_FIND_COMMAND | tee $VAR_TMP_FILE_COMMAND_OUTPUT"
+#
+PrintMessageV2 "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" $(which find) $VAR_FIND_COMMAND
+#
+PrintMessageV2
 #
 if [[ $GLOBAL_VAR_DRY_RUN -eq 1 ]]; then
-    PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "No output is generated because of this search is a Dry Run. Exiting..."
+    PrintMessageV2 "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "No output is generated because of this search is a Dry Run. Exiting..."
     exit 0
 fi
 #
 VAR_COUNT_FOUND_ITEMS=$(cat $VAR_TMP_FILE_COMMAND_OUTPUT | grep -Ev "$SUPPRESS_STRING" | wc -l)
 VAR_COUNT_FOUND_ERRORS=$(cat $VAR_TMP_FILE_COMMAND_OUTPUT | grep -E "$SUPPRESS_STRING" | wc -l)
-PrintMessage "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Found $(echo $VAR_COUNT_FOUND_ITEMS) files/folders and $(echo $VAR_COUNT_FOUND_ERRORS) errors/warnings in '$VAR_SEARCH_DIR' for '$VAR_SEARCH_QUERY'"
+PrintMessageV2 "INFO" "$VAR_UTILITY" "$VAR_UTILITY_SCRIPT" "Found $(echo $VAR_COUNT_FOUND_ITEMS) files/folders and $(echo $VAR_COUNT_FOUND_ERRORS) errors/warnings in '$VAR_SEARCH_DIR' for '$VAR_SEARCH_QUERY'"
