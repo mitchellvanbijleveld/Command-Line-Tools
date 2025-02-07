a0ce3d8d234f10d7fac00c63372cbcec016c3b21
Mitchell van Bijleveld <hello@mitchellvanbijleveld.dev>
2025-02-07 14:10:08 +0100

    Do not process flags (--FLAG)

--- .mitchellvanbijleveld/Find/UtilityScriptFilePath.bash
+++ .mitchellvanbijleveld/Find/UtilityScriptFilePath.bash
@@ -88,6 +88,11 @@ PrintMessage "DEBUG" "$BIN_HELPER_UTILITY" "$BIN_HELPER_UTILITY_SCRIPT" "Find Ut
 until [[ -f $VAR_UTILITY_SCRIPT_FILE_PATH ]] || [[ $# -eq 0 ]]; do
     found_file=0; found_folder=0; last_search=$1
     #
+    if [[ $1 == "--"* ]]; then
+        PrintMessage "DEBUG" "$BIN_HELPER_UTILITY" "$BIN_HELPER_UTILITY_SCRIPT" "Skip flag '$1'..."
+        shift; continue
+    fi
+    #
     PrintMessage "DEBUG" "$BIN_HELPER_UTILITY" "$BIN_HELPER_UTILITY_SCRIPT" "Find Utility Folder Path '$1' in '$VAR_UTILITY_FOLDER_PATH'..."
     find_folder=$(find -L $VAR_UTILITY_FOLDER_PATH -maxdepth 1 -iname $1 -type d)
     #
